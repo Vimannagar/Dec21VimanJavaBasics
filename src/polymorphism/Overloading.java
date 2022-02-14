9c09a6ad9a86fa27876a41b5202341c95b9c348a
Gourav Jawale <velocity.ctc@gmail.com>
2022-02-14 10:19:33 +0530

    Overloading

---
+++ src/polymorphism/Overloading.java
@@ -0,0 +1,67 @@
+package polymorphism;
+
+public class Overloading {
+	
+	public static void m1()
+	{
+		System.out.println("m1 method with no argument");
+		
+	}
+	
+	public static void m1(int i)
+	{
+		System.out.println("m1 second static method");
+	}
+	
+	public void m1(boolean b)
+	{
+		System.out.println("m1 method with boolean argument");
+		
+	}
+	
+	public void m1(int i, boolean b )
+	{
+		System.out.println("m1 method with int and then boolean");
+	}
+	
+	public void m1(boolean b, int i )
+	{
+		System.out.println("m1 method with int and then boolean");
+	}
+	
+	
+	public int m1(char c)
+	{
+		System.out.println("method m1 with return type");
+		
+		return 50;
+	}
+	
+	
+	
+	public static void nameChange(int adharno, String updatedname, String marksheet)
+	{
+		System.out.println("name changed");
+	}
+	
+	public static void nameChange(int adharno, String updatedname, String marksheet, String marriagecert)
+	{
+		System.out.println("name changed after marriage");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		m1();
+		
+		m1(50);
+		
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ src/polymorphism/Test.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class Test extends Overloading{
+	
+	
+	
+	public void m1(String s)
+	{
+		System.out.println("m1 method with string argument");
+	}
+
+}
